4f3bb00db2a8af6290d1f7267c5d4cc6ad4bd767
ramonefoster <ramonefoster@gmail.com>
2023-02-03 21:50:41 -0300

    v1.0 - fix urls

--- weatherBackend/urls.py
+++ weatherBackend/urls.py
@@ -20,5 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('docs/', TemplateView.as_view(template_name='index.html')),
+    path('weather/<str:dates>/', TemplateView.as_view(template_name='index.html'))
 ]
